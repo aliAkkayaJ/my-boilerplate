fcd538c224813de7856675e40030c0ac739f3b67
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-03-05 17:49:03 +0300

    utils-generateTokencrcreated

---
+++ utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+//frontend should keep the token in the localstorage
+module.exports = generateToken;
