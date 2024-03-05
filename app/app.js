fe0e22bcb81c87f6f0f5d0dc381c49b84ba94cd7
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-03-05 18:34:05 +0300

    error handler middleware

--- app/app.js
+++ app/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const adminRouter = require("./routes/adminRouter");
+const {
+  notFoundErr,
+  globalErrHandler,
+} = require("../middlewares/globalErrHandler");
+
+//app
 const app = express();
 
 // Middleware
 app.use(express.json());
 
 // Routes
+app.use("/api/v1/admins", adminRouter);
+
+// Error Handler
+app.use(notFoundErr);
+app.use(globalErrHandler);
+
 module.exports = app;
