daf4dee842329200cf5fffdf73414defaae5de7d
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-05-17 20:37:40 +0300

    error handler güncellendi

--- middlewares/globalErrHandler.js
+++ middlewares/globalErrHandler.js
@@ -1,12 +1,11 @@
-const globalErrHandler = (err, req, res, next) => {
-  //status
-  //message
-  //stack-> that shows which line include error
+export const globalErrHandler = (err, req, res, next) => {
+  // Hatanın bilgilerini al
   const stack = err.stack;
   const message = err.message;
-  const status = err.status ? err.status : "failed";
+  const status = err.status ? err.status : false;
   const statusCode = err.statusCode ? err.statusCode : 500;
 
+  // Hata bilgilerini JSON olarak yanıtla
   res.status(statusCode).json({
     status,
     message,
@@ -14,10 +13,9 @@ const globalErrHandler = (err, req, res, next) => {
   });
 };
 
-//Not found
-const notFoundErr = (req, res, next) => {
+// Bulunamayan kaynak için hata işleyici
+export const notFoundErr = (req, res, next) => {
   const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  // Hata nesnesini sonraki işleyiciye ilet
   next(err);
 };
-
-module.exports = { globalErrHandler, notFoundErr };
